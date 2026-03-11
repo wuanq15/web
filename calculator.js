f626e53af682eeaefc23b803dbdc39358aecf996
wuanq15 <samurai1501205@gmail.com>
2026-03-11 08:47:31 +0700

    Initial calculator base for conflicts

---
+++ calculator.js
@@ -0,0 +1,35 @@
+class Calculator {
+    constructor() {
+        this.value = 0;
+        this.history = [];
+    }
+
+    add(number) {
+        // basic addition
+        let prev = this.value;
+        this.value += number;
+        return this.value;
+    }
+
+    subtract(number) {
+        // basic subtraction
+        this.value -= number;
+        return this.value;
+    }
+
+    multiply(number) {
+        // basic multiplication
+        this.value *= number;
+        return this.value;
+    }
+
+    divide(number) {
+        // basic division
+        if (number === 0) {
+            console.error("Divide by zero");
+            return null;
+        }
+        this.value /= number;
+        return this.value;
+    }
+}
